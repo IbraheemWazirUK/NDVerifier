2a2c6097d1c3d0adf48e05010512388af285922f
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2020-11-30 20:53:31 +0000

    implemented and intro and and elimination

--- src/expression.py
+++ src/expression.py
@@ -1,8 +1,20 @@
 class Exp():
+	def eliminate(self):
+		return []
 	def __init__(self, operands):
 		self.operands = operands
+	def __eq__(self, other):
+		return (self.operands == other.operands) and (type(self) == type(other)) 
 
 class And(Exp):
+	def eliminate(self):
+		return [self.operands[0], self.operands[1]]
+	def introduction(self, env, cur_true_exps):
+		if (self.operands[0] in cur_true_exps) and (self.operands[1] in cur_true_exps):
+			env[-1].append(self)
+			cur_true_exps.append(self)
+			return true
+		return false
 	def __str__(self):
 		return '(' + self.operands[0].__str__() + ' and ' +  self.operands[1].__str__() + ')'
 
@@ -22,5 +34,4 @@ class Var(Exp):
 	def __str__(self):
 		return '(' + self.operands[0].__str__() + ')'
 	
-
 		
